70abf601f9d47584659868da1fd448bdae1d0a00
andrew-secret <andre.rusakow@jimdo.com>
2017-10-03 23:43:52 +0200

    add clientList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import {Button} from './button/button';
-import Timer from './Timer/Timer';
-import './App.scss';
+import React, { Component } from "react";
+import { Button } from "./button/button";
+import Timer from "./Timer/Timer";
+import ClientList from "./clientList/clientList";
+import "./App.scss";
 
-const clientList = [
+const clients = [
   {
     id: 0,
-    client: 'Aurelo',
-    project: 'design something',
-    date: '20. September 2017',
-    time: '02:30:15',
-    earned: 120,
+    client: "Aurelo"
   },
   {
     id: 1,
-    client: 'Nikdin',
-    project: 'develop something',
-    date: '20. September 2017',
-    time: '02:30:15',
-    earned: 120,
+    client: "Nikdin"
   },
+  {
+    id: 2,
+    client: "WakeSys"
+  }
 ];
 
 class App extends Component {
@@ -27,75 +24,82 @@ class App extends Component {
     super(props);
 
     this.state = {
-      clientList,
+      clients,
       timeElapsed: 0,
       isRunning: false,
+      currentClient: ''
     };
+    this.handleChange = this.handleChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.startTimer = this.startTimer.bind(this);
   }
 
   onDismiss(id) {
-    const isNotId = (item) => item.id !== id;
+    const isNotId = item => item.id !== id;
     const updatedClientList = this.state.clientList.filter(isNotId);
     this.setState({
       clientList: updatedClientList
-    })
+    });
   }
 
-  getSeconds = () => (`0${this.state.timeElapsed % 60}`).slice(-2);
-  getMinutes = () => (`0${Math.floor(this.state.timeElapsed / 60)}`).slice(-2);
+  getSeconds = () => `0${this.state.timeElapsed % 60}`.slice(-2);
+  getMinutes = () => `0${Math.floor(this.state.timeElapsed / 60)}`.slice(-2);
   getHours = () => `0${Math.floor(this.state.timeElapsed / 600)}`;
 
+  handleChange(event) {
+    this.setState({
+      currentClient: event.target.value
+    });
+    console.log('event', event.target.value);
+  }
 
-   startTimer = () => {
+  startTimer = () => {
     this.interval = setInterval(() => {
-      this.setState({ 
+      this.setState({
         timeElapsed: this.state.timeElapsed + 1,
         isRunning: true
-      })
-     }, 1000);
-  }
+      });
+    }, 1000);
+  };
 
   stopTimer = () => {
     clearInterval(this.interval);
     this.setState({
       isRunning: false
-    })
-  }
+    });
+  };
 
   reset = () => {
     clearInterval(this.interval);
     this.setState({
       timeElapsed: 0,
       isRunning: false
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Timer timer={this.state.timer}
+        <Timer
+          timer={this.state.timer}
           startTimer={this.startTimer}
           stopTimer={this.stopTimer}
           reset={this.reset}
           hours={this.getHours()}
           isRunning={this.state.isRunning}
           minutes={this.getMinutes()}
-          seconds={this.getSeconds()}/>
-        {this.state.clientList.map((item) => <div key={item.id}>
-          {item.client}
-          <Button
-            onClick={() => this.onDismiss(item.id)}>
-          </Button>
-        </div>)}
-        <Button
-          onClick={this.reset}
-          label="reset">
-        </Button>
+          seconds={this.getSeconds()}
+        />
+        <ClientList
+          currentClient={this.state.currentClient}
+          clients={this.state.clients}
+          handleChange={this.handleChange}/>
+        <Button onClick={this.reset} label="reset" />
       </div>
     );
   }
 }
 
 export default App;
+
+
