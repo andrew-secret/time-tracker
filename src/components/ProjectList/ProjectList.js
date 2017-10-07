b8e2f450da548cebe59be111ec40bb02f02e1a9e
andrew-secret <andre.rusakow@jimdo.com>
2017-10-07 22:54:57 +0200

    implmenent projectList component

---
+++ src/components/ProjectList/ProjectList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './ProjectList.scss';
+import ProjectListItem from '../ProjectListItem/ProjectListItem';
+
+const ProjectList = props => {
+  console.log("isRunning", props.isRunning);
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles.projectList}>
+        {props.projects.map(project => (
+          <ProjectListItem 
+            hours={props.hours}
+            minutes={props.minutes}
+            seconds={props.seconds}
+            id={project.id}
+            client={project.client}
+            timeElapsed={project.timeElapsed}
+            projectDescription={project.projectDescription}/>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectList;
